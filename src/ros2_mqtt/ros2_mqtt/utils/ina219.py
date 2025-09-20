6f451ec15df54e6e4fcfbfd78038a601aac75d60
LucasLagoeiro <llagoeiro@outlook.com.br>
2025-09-20 11:07:48 -0400

    Update: Now INA219 interface has a class && the voltage is publishing on two topics (ROS2 topic and MQTT topic)

--- src/ros2_mqtt/ros2_mqtt/utils/ina219.py
+++ src/ros2_mqtt/ros2_mqtt/utils/ina219.py
@@ -1,27 +1,18 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)       # I²C bus 1
-addr = 0x41                # endereço do INA219
+class INA219:
+    """Class to interface with the INA219 sensor via I2C."""
+    def __init__(self, address=0x41, busnum=1):
+        self._address = address
+        self._bus = smbus.SMBus(busnum)
 
+    def read_voltage(self):
+        raw = self._bus.read_word_data(self._address, 0x02)
+        val = self._swap16(raw) 
+        vbus_raw = (val >> 3) & 0x1FFF # bits [15..13] = flags, bits [12..3] = VBUS
+        volts = vbus_raw * 0.004   # each LSB = 4 mV
+        return volts
 
-
-def swap16(val):
-    return ((val & 0xFF) << 8) | ((val >> 8) & 0xFF)
-
-
-
-while(True):
-    raw = bus.read_word_data(addr, 0x02)
-    val = swap16(raw)
-
-    print("Valor original:", raw)
-    print("Valor ajustado:", val)
-
-    # bits [15..13] = flags, bits [12..3] = VBUS
-    vbus_raw = (val >> 3) & 0x1FFF
-    volts = vbus_raw * 0.004   # cada LSB = 4 mV
-
-    print(f"Tensão do barramento = {volts:.3f} V")
-    time.sleep(1)
-    
+    def _swap16(self, val):
+        return ((val & 0xFF) << 8) | ((val >> 8) & 0xFF)
